35c7bc947b6658a97ced257df31718903eafbe62
youyeongjin <show5116@naver.com>
2022-04-28 16:07:32 +0900

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mighty-WebReport Project
+
+
+Command Line
+```
+$ npm install -g @vue/cli
+$ vue create front-end
+$ cd front-end
+$ npm run serve
+```
---
+++ src/main/java/com/ibs/mighty/mightywebreport/MightyWebreportApplication.java
@@ -0,0 +1,13 @@
+package com.ibs.mighty.mightywebreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MightyWebreportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MightyWebreportApplication.class, args);
+    }
+
+}
