ecf8a91d6c96a92c5fb856c12112e00426573561
youyeongjin <show5116@naver.com>
2022-05-09 14:32:30 +0900

    CHORE : change member entity

--- src/main/java/com/mighty/webreport/domain/entity/Member.java
+++ src/main/java/com/mighty/webreport/domain/entity/Member.java
@@ -23,11 +23,49 @@ public class Member {
     @Column(name = "USER_ID")
     private String userId;
 
+    private String PASSWORD_MD5;
+
+    private String password;
+
     @Column(name = "USER_NAME")
     private String userName;
 
     @Column(name = "PLANT")
     private String plant;
 
+    @Column(name = "EMAIL_ID")
+    private String email;
 
+    @Column(name = "PHONE_NO")
+    private String phone;
+
+    @Column(name = "ROLE_ID")
+    private String role;
+
+    private String REG_USER;
+    private String REG_TIME;
+    private String LANGUAGE_TYPE;
+    private String EXTERNAL_TYPE;
+    private String EXTERNAL_FLAG;
+    private String EXPIRE_DATE;
+    private String EXPAND_FLAG5;
+    private String EXPAND_FLAG4;
+    private String EXPAND_FLAG3;
+    private String EXPAND_FLAG2;
+    private String EXPAND_FLAG1;
+    private String EXPAND_FIELD9;
+    private String EXPAND_FIELD8;
+    private String EXPAND_FIELD7;
+    private String EXPAND_FIELD6;
+    private String EXPAND_FIELD5;
+    private String EXPAND_FIELD4;
+    private String EXPAND_FIELD3;
+    private String EXPAND_FIELD2;
+    private String EXPAND_FIELD10;
+    private String EXPAND_FIELD1;
+    private String EMAIL_ID;
+    private String DESCRIPTION;
+    private String DEPARTMENT;
+    private String ADMISSION;
+    private String ACCESS_GROUP_ID;
 }
