ee5064cb51bf6d530c3edd9e44ee9bc938589a69
youyeongjin <show5116@naver.com>
2022-05-09 14:31:11 +0900

    FEAT : add global style

---
+++ frontend/src/styles/color.ts
@@ -0,0 +1,13 @@
+const color = {
+    darkBlue: '#1d3557',
+    grayBlue: '#457b9d',
+    lightBlue: '#a8dadc',
+    green: '#f1faee',
+    ivory: '#fbfefb',
+    red: '#d22227',
+    lightgray: '#e7e7e7',
+    darkgray: '#555555',
+    gray: '#999',
+}
+
+export default color;
